b2018b83495dcecc60b15163e51f5253d5b70b62
yori21 <mail@alexochs.de>
2021-10-16 22:30:36 +0200

    implement game manager

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -46,7 +46,7 @@ public class GameManager : MonoBehaviour
 
     public void StartNewGame()
     {
-        LoadScene("GameManagerScene");
+        LoadScene("Game");
         this.status = "INGAME";
     }
 
