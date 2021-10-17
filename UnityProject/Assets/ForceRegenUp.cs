900d0621148513c82e8c363fe8c60a50ae149c96
scout507 <70096457+scout507@users.noreply.github.com>
2021-10-17 12:40:00 +0200

    PowerUps completed? (#31)
    Co-authored-by: konstyy <64797633+konstyy@users.noreply.github.com>

---
+++ UnityProject/Assets/ForceRegenUp.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceRegenUp : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    public float multiplier = 1.4f;
+    public float duration = 5f;
+
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player")){
+            StartCoroutine(Pickup(other));//Start Coroutine wenn added
+        }
+    }
+    
+    IEnumerator Pickup(Collider player){ //IEnumerator
+        Debug.Log("PowerUP picked up!");
+        //Spawn a cool effect
+        GameObject clone = Instantiate(pickupEffect, transform.position, transform.rotation);
+        ParticleSystem.MainModule particle = clone.GetComponent<ParticleSystem>().main;
+
+        //Apply effect to the player
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+
+  
+        player.GetComponent<MoveSword>().forceRegen = 20;
+        yield return new WaitForSeconds(duration);
+        player.GetComponent<MoveSword>().maxForce = 5;
+
+
+        //Destroy the Object
+        Destroy(clone, particle.duration);
+        Destroy(gameObject);
+        
+    }
+}
---
+++ UnityProject/Assets/HealthUp.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUp : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    public float multiplier = 1.4f;
+    public float duration = 5f;
+
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player")){
+            StartCoroutine(Pickup(other));//Start Coroutine wenn added
+        }
+    }
+    
+    IEnumerator Pickup(Collider player){ //IEnumerator
+        Debug.Log("PowerUP picked up!");
+        //Spawn a cool effect
+        GameObject clone = Instantiate(pickupEffect, transform.position, transform.rotation);
+        ParticleSystem.MainModule particle = clone.GetComponent<ParticleSystem>().main;
+
+        //Apply effect to the player
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+
+  
+        player.GetComponent<MoveSword>().hp = player.GetComponent<MoveSword>().maxhp;
+        yield return new WaitForSeconds(duration);
+
+
+        //Destroy the Object
+        Destroy(clone, particle.duration);
+        Destroy(gameObject);
+        
+    }
+}
